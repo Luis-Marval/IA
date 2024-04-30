e20836d1ddfe1ca9dfbb9562f6c187ec14d9e2e2
Luis C. <Luismarval5a@hotmail.com>
2024-04-30 15:42:22 -0400

    Base de la IA

---
+++ Frontend/js/mains.js
@@ -0,0 +1,12 @@
+var chat = document.getElementById("chatbox")
+var btn = document.getElementById("send-btn")
+
+btn.addEventListener("click",()=>{    
+    var element = document.createElement('li');
+    var parrafo = document.createElement('p');
+    var Info = document.getElementById("Info").value;
+    element.classList.add("chat","outgoing")
+    parrafo.innerHTML = Info;
+    element.appendChild(parrafo);
+    chat.appendChild(element);
+    })
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Proyecto de IA
+Este proyecto tiene el objetivo crear una Inteligencia artifical para el CINU, de la UNEFA
